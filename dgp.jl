32a08962ddad7338be23f0df6ef890cfdd254278
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-09-26 19:36:38 -0700

    add simulateAR function

--- dgp.jl
+++ dgp.jl
@@ -24,3 +24,35 @@ function gen_rand_cov(N,μ,σ)
     return cov_mat
 end 
 
+@doc """
+    
+    gen_rand_cov(N,μ,σ)
+
+Description:
+Generate a random covariance matrix. 
+
+Inputs:
+- N = number of variables
+- μ = expected covariance 
+- σ = expected std. deviation of covariance
+"""
+function univariateAR(obs_num = 1, parameters = [0.0,0.0], error_distribution = Normal())
+
+    lag_num = length(parameters) - 1
+    data = convert(Array{Float64,1}, 1:obs_num)
+    disturbance = rand(error_distribution, obs_num)
+
+    for i in 1:obs_num
+        if i > lag_num 
+            data[i] =  parameters[1] + 
+                        reverse(parameters[2:length(parameters)])'*data[(i-lag_num):(i-1)]+ 
+                        disturbance[i]
+        else 
+            data[i] = parameters[1] + 
+                        reverse(parameters[2:length(parameters)])[1:(i-1)]'*data[1:(i-1)] +
+                        disturbance[i]
+        end 
+    end 
+
+    return data
+end 
