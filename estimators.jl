4d3bda79df14ddf54a9900f8fc7921678770b88c
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-11-14 13:16:35 -0800

    Finish Kalman filter function

--- estimators.jl
+++ estimators.jl
@@ -5,10 +5,10 @@ using LinearAlgebra
 ######################
 @doc """
     
-    kalmanFilter(data, H, A, F, μ, R, Q)
+    kalmanFilter(data, H, A, F, μ, R, Q, Z)
 
 Description: 
-Generate data from a DGP in state space form.
+Apply Kalman filter to observed data. 
 Measurement Equation:   
     y_{t} = H_{t} β_{t} + A z_{t} + e_{t} 
 Transition Equation:    
@@ -28,7 +28,45 @@ Inputs:
 - Q         = covariance matrix on state disturbance
 - Z         = covariance matrix on predetermined var vector 
 """
-function kalmanFilter(data, H, A, F, μ, R, Q)
-    data_filtered = 0
-    return data_filtered 
+function kalmanFilter(data_y, data_z, H, A, F, μ, R, Q, Z)
+
+    # Save number of observations 
+    num_obs = size(data_y)[1]
+
+    # Empty filtered data matrices 
+    data_filtered_y = similar(data_y)
+    data_filtered_β = zeros(num_obs, size(Q)[1])
+
+    # Initialize β_pred and P_pred 
+    β_pred_laglag = inv(I - F) * μ
+    P_pred_laglag = zeros(size(Q)[1],size(Q)[1])
+
+    for t = 1:num_obs
+        # Save current obs of z 
+        z = data_z[t,:]
+
+        # Prediction 
+        β_pred_lag = μ + F * β_pred_laglag
+        P_pred_lag = F * P_pred_laglag * transpose(P) + Q
+        y_pred_lag = H * β_pred_lag + A * z
+        η_pred_lag = y - y_pred_lag
+        f_pred_lag = H * P_pred_lag * transpose(H) + R
+
+        # Updating 
+        K = P_pred_lag * transpose(H) * inv(f_pred_lag)
+        β_pred = β_pred_lag + K * η_pred_lag
+        P_pred = P_pred_lag - K * H * P_pred_lag
+
+        # Save data 
+        data_filtered_y[t,:] = y_pred_lag 
+        data_filtered_β[t,:] = β_pred 
+
+        # Lag the predictions 
+        β_pred_laglag = β_pred
+        P_pred_laglag = P_pred
+    end
+
+    # Returned filtered series 
+    # for obs variable and state 
+    return data_filtered_y, data_filtered_β
 end 
