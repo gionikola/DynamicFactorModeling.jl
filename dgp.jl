1b2a960a594b0e7c0b844d6d1bdbec1b4de94be6
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-11-14 02:07:57 -0800

    Update simulateStateSpaceModel()

--- dgp.jl
+++ dgp.jl
@@ -166,37 +166,59 @@ Inputs:
 """
 function simulateStateSpaceModel(num_obs, H, A, F, μ, R, Q, Z)
     
+    # Create empty data storage matrices 
     data_y  = zeros(num_obs,size(H)[1])
     data_z  = zeros(num_obs,size(Z)[1])
 
+    # Initialize β and y 
     β0 = inv(I-F)*μ
     y0 = H*β0
 
+    # Initialize z
     if Z == zeros(size(Z)[1],size(Z)[1])
         z0 = zeros(size(Z)[1])
     else 
         z0 = rand( MvNormal( zeros( size(Z)[1] ), Z ) )
     end 
 
+    # Save first observations of y and z
     data_y[1,:] = y0
     data_z[1,:] = z0
 
+    # Initialize β lag for recursion 
     β_lag       = β0
 
+    # Recursively generate data
     for t in 2:num_obs
-        v           = rand(MvNormal(0*[1:size(Q)[1]], Q))
+        # Draw transition distrubance 
+        if Q == zeros(size(Q)[1],size(Q)[1])
+            v = zeros(size(Q)[1])
+        else 
+            v = rand(MvNormal(0*[1:size(Q)[1]], Q))
+        end 
+        # Record new state observation 
         β           = μ + F*β_lag + v
+        # Draw new z observation
         if Z == zeros(size(Z)[1],size(Z)[1])
             z = zeros(size(Z)[1])
         else 
             z = rand( MvNormal( zeros( size(Z)[1] ), Z ) )
         end 
-        e = rand(MvNormal(0*[1:size(R)[1]], R))
+        # Draw measurement distrubance 
+        if R == zeros(size(R)[1],size(R)[1])
+            e = zeros(size(R)[1])
+        else 
+            e = rand(MvNormal(0*[1:size(R)[1]], R))
+        end 
+        # Record new measurement observation 
         y           = H*β + A*z + e
+        # Save generated data 
         data_y[t,:] = y
         data_z[t,:] = z
+        # Update β lag for recursion 
         β_lag       = β
     end 
 
+    # Return data 
     return data_y, data_z 
 end 
