a65a36d9bc13cf01c0a0754159f3c9cac7a89979
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-11-14 01:02:41 -0800

    Create state space DGP simulator funciton

--- dgp.jl
+++ dgp.jl
@@ -135,3 +135,34 @@ function firstComponentFactor(data)
 
     return factor 
 end 
+
+######################
+######################
+######################
+@doc """
+
+    stateSpaceModel(num_obs, H, A, F, μ, R, Q)
+
+Description: 
+Generate data from a DGP in state space form.
+Measurement Equation:   
+    y_{t} = H_{t} β_{t} + A z_{t} + e_{t} 
+Transition Equation:    
+    β_{t} = μ + F β_{t-1} + v_{t}
+    e_{t} ~ i.i.d.N(0,R)
+    v_{t} ~ i.i.d.N(0,Q)
+    E(e_t v_s') = 0
+
+Inputs: 
+- num_obs   = number of observations
+- H         = measurement eq. state coef. matrix
+- A         = measurement eq. exogenous coef. matrix
+- F         = state eq. companion matrix
+- μ         = state eq. intercept term
+- R         = covariance matrix on measurement disturbance
+- Q         = covariance matrix on state disturbance
+"""
+function simulateStateSpaceModel(num_obs, H, A, F, μ, R, Q)
+    data  = 0
+    return data
+end 
