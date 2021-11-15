b7c9639372a4724a7de8b1875960ce38fc5e14b9
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-11-14 23:21:51 -0800

    Create preliminary Kalman smoother function

--- estimators.jl
+++ estimators.jl
@@ -71,3 +71,87 @@ function kalmanFilter(data_y, data_z, H, A, F, μ, R, Q, Z)
     # for obs variable and state 
     return data_filtered_y, data_filtered_β
 end 
+
+######################
+######################
+######################
+@doc """
+    
+    kalmanSmoother(data, H, A, F, μ, R, Q, Z)
+
+Description: 
+Apply Kalman smoother to observed data. 
+Measurement Equation:   
+    y_{t} = H_{t} β_{t} + A z_{t} + e_{t} 
+Transition Equation:    
+    β_{t} = μ + F β_{t-1} + v_{t}
+    e_{t} ~ i.i.d.N(0,R)
+    v_{t} ~ i.i.d.N(0,Q)
+    z_{t} ~ i.i.d.N(0,Z)
+    E(e_t v_s') = 0
+
+Inputs: 
+- data      = observed data 
+- H         = measurement eq. state coef. matrix
+- A         = measurement eq. exogenous coef. matrix
+- F         = state eq. companion matrix
+- μ         = state eq. intercept term
+- R         = covariance matrix on measurement disturbance
+- Q         = covariance matrix on state disturbance
+- Z         = covariance matrix on predetermined var vector 
+"""
+function kalmanSmoother(data_y, data_z, H, A, F, μ, R, Q, Z)
+
+    # Save number of observations 
+    num_obs = size(data_y)[1]
+
+    # Initialize β_pred and P_pred 
+    β_pred_laglag = inv(I - F) * μ
+    P_pred_laglag = ones(size(Q)[1], size(Q)[1])
+
+    #####################
+    ### Kalman Filter ###
+    #####################
+    for t = 1:num_obs
+        # Save current obs of z & y
+        z = data_z[t, :]
+        y = data_y[t, :]
+
+        # Prediction 
+        β_pred_lag = μ + F * β_pred_laglag
+        P_pred_lag = F * P_pred_laglag * transpose(F) + Q
+        y_pred_lag = H * β_pred_lag + A * z
+        η_pred_lag = y - y_pred_lag
+        f_pred_lag = H * P_pred_lag * transpose(H) + R
+
+        # Updating 
+        K = P_pred_lag * transpose(H) * inv(f_pred_lag)
+        β_pred = β_pred_lag + K * η_pred_lag
+        P_pred = P_pred_lag - K * H * P_pred_lag
+
+        # Lag the predictions 
+        β_pred_laglag = β_pred
+        P_pred_laglag = P_pred
+    end
+
+    # Empty filtered data matrices 
+    data_smoothed_β = zeros(num_obs, size(Q)[1])
+
+    # Last filtered estimates for β and P
+    data_smoothed_β[num_obs, :] = β_pred
+
+    #######################
+    ### Kalman Smoother ###
+    #######################
+    for i = 1:(num_obs-1)
+        
+    end
+
+    # Returned filtered series 
+    # for obs variable and state 
+    return data_smoothed_β
+end 
+
+######################
+######################
+######################
