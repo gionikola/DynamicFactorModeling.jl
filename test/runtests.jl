5131dec87f73a396f6028405d97f69fea35e0e39
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-12-10 00:49:39 -0800

    Add simulation and Kalman filter tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,16 @@ using DynamicFactorModeling
 using Test
 
 @testset "DynamicFactorModeling.jl" begin
-    # Write your tests here.
+    num_obs = 100
+    H = [1.0 0.0]
+    A = [0.0][:, :]
+    F = [0.3 0.5; 1.0 0.0]
+    μ = [0.0, 0.0]
+    R = [0.0][:, :]
+    Q = [1.0 0.0; 0.0 0.0]
+    Z = [0.0][:, :]
+
+    @test data_y, data_z, data_β = simulateStateSpaceModel(num_obs, H, A, F, μ, R, Q, Z)
+    @test data_filtered_y, data_filtered_β, Pttlag, Ptt = kalmanFilter(data_y, data_z, H, A, F, μ, R, Q, Z)
+    
 end
