1e0120a63562c479bcbef0cebec7e0a29990e2f1
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-12-09 23:17:35 -0800

    Organize as package and export functions

--- src/DynamicFactorModeling.jl
+++ src/DynamicFactorModeling.jl
@@ -1,5 +1,26 @@
 module DynamicFactorModeling
 
-# Write your package code here.
+######################
+# Import packages 
+using LinearAlgebra
+using Random
+using Distributions
+using PDMats
+using PDMatsExtras
+using ShiftedArrays
+using Parameters
+
+######################
+# Export package objects 
+export simulateStateSpaceModel, SSModelParameters,
+    kalmanFilter, kalmanSmoother,
+    dynamicFactorGibbsSampler,
+    staticLinearGibbsSampler, staticLinearGibbsSamplerRestrictedVariance,
+    autocorrErrorRegGibbsSampler
+
+######################
+# Include all package scripts 
+include("dgp.jl")
+include("estimators.jl")
 
 end
