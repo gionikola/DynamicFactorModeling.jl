2ffed8c3ef04e1478d5bd2eb40b199c86f41eb56
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-12-20 12:35:50 -0800

    Create `ar_LJ` function to estimate hyperparameter estimates

--- src/otrok_whiteman/ow_twofactor_estimator.jl
+++ src/otrok_whiteman/ow_twofactor_estimator.jl
@@ -10,6 +10,137 @@
 ######################
 ######################
 ######################
+function ar_LJ(y, x, p, b0_, B0__, r0_, R0__, v0_, d0_, b0, s20, phi0, xvar, nfc, facts, capt, nreg, Size)
+
+    local xst, yst, b1, phi1
+
+    n = capt
+    signmax1 = 1
+    signbeta1 = 1
+    signmax2 = 1
+    signbeta2 = 0
+
+    while signbeta1 + signbeta2 >= 1
+
+        # generation of phi1 
+        yp = y[1:p, 1]          # the first p observations 
+        xp = x[1:p, :]
+
+        e = y - x * b0
+        e1 = e[(p+1):n, 1]
+        ecap = zeros(n, p)
+
+        for j = 1:p
+            ecap[:, j] = lag(e, j, default = 0.0)
+        end
+
+        ecap = ecap[(p+1):n, :]
+
+        V = invpd(R0__ .+ inv(s20) * (transp_dbl(ecap) * ecap))
+        phihat = V * (R0__ * r0_ + inv(s20) * (transp_dbl(ecap) * e1))
+
+        #phi1 = phihat + transp_dbl(cholesky(V)) * randn(p, 1)       # candidate draw 
+        phi1 = rand(MvNormal(vec(phihat), PSDMat(Matrix(V))))
+
+        coef = [-reverse(vec(phi1), dims = 1); 1]                      # check stationarity 
+        root = roots(Polynomial(coef))
+        rootmod = abs.(root)
+        accept = min(rootmod...) >= 1.001             # all the roots bigger than 1 
+
+        if accept == 0
+            phi1 = phi0
+        else
+            #=
+            ##### THE FOLLOWING CODE HAS POSITIVE-DEFINITENESS ISSUES 
+            ##### IN SIGMA1 
+            sigma1 =  Hermitian(sigmat(vec(phi1), p))               # numerator of acceptance prob 
+            sigroot = cholesky(sigma1)
+            p1 = inv(sigroot)'
+            ypst = p1 * yp
+            xpst = p1 * xp
+            d = det(p1' * p1)
+            psi1 = (d^(1 / 2)) * exp(-0.5 * (ypst - xpst * b0)' * (ypst - xpst * b0) / s20)
+            =#
+            sigma1 = Hermitian(sigmat(vec(phi1), p))               # numerator of acceptance prob 
+            d = Complex(det(sigma1))
+            psi1 = (d^(-0.5)) * exp((-0.5 / s20) * (transp_dbl(yp - xp * b0)*invpd(sigma1)*(yp-xp*b0))[1])
+
+            #=
+            sigma1 = Hermitian(sigmat(phi0, p))
+            sigroot = cholesky(sigma1)
+            p1 = inv(sigroot)'
+            ypst = p1 * yp
+            xpst = p1 * xp
+            d = det(p1' * p1)
+            psi0 = (d^(1 / 2)) * exp(-0.5 * (ypst - xpst * b0)' * (ypst - xpst * b0) / s20)
+            =#
+            sigma1 = Hermitian(sigmat(vec(phi0), p))               # numerator of acceptance prob 
+            d = Complex(det(sigma1))
+            psi0 = (d^(1 / 2)) * exp((-0.5 / s20) * (transp_dbl(yp - xp * b0)*invpd(sigma1)*(yp-xp*b0))[1])
+
+            if psi0 == 0
+                accept = 1
+            else
+                u = rand(1)[1]
+                accept = u <= real(psi1) / real(psi0)
+            end
+            phi1 = real(phi1) * accept + real(phi0) * (1 - accept)
+        end
+
+        # generation of beta 
+        sigma = Hermitian(sigmat(phi1, p))              # sigma = sigroot' * sigroot 
+        sigroot = cholesky(sigma)                       # signber2v = p1' * p1 
+        p1 = transp_dbl(inv(sigroot))
+        ypst = p1 * yp
+        xpst = p1 * xp
+        yst = [ypst; gendiff(y, phi1)]
+        xst = [xpst; gendiff(x, phi1)]
+
+        V = invpd(B0__ + s20^(-1) * xst' * xst)
+        bhat = V * (B0__ * b0 + s20^(-1) * xst' * yst)
+        #b1 = bhat + cholesky(V)' * randn(nreg, 1)          # the draw of beta 
+        b1 = rand(MvNormal(vec(bhat), PSDMat(Matrix(V))))
+
+        signbeta1 = (b1[2, 1] <= 0.0) * (xvar == 1)
+        signmax1 = signmax1 + (1 * signbeta1)
+        signbeta2 = (b1[3, 1] <= 0.0) * ( ((xvar-1)/Size) == floor((xvar-1)/Size))
+        signmax2 = signmax2 + (1 * signbeta2) 
+
+        if signmax1 >= 100
+            facts[:, 1] = (-1) * facts[:, 1]
+            x[:, 2] = facts[:, 1]
+            signmax1 = 1
+        end
+        if signmax2 >= 100
+            facts[:, nfc] = (-1)*facts[:, nfc]
+            x[:,3] = facts[:, nfc]
+            signmax2 = 1 
+        end 
+    end
+
+    # generation of s2 
+    nn = n + v0_
+    d = d0_ + transp_dbl(yst - xst * b1) * (yst - xst * b1)
+    c = rnchisq(nn)
+    t2 = c ./ d
+    s21 = 1 ./ t2
+    b0 = b1
+    s20 = s21[1, 1]
+
+    return b0, s20, phi1, facts
+end 
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
 function OWTwoFactorEstimator(data, prior_dim)
 
     y = data                    # save data in new matrix 
