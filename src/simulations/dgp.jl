93d121f086a71ec8a3d102578b6095423a74c51d
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-12-21 14:19:25 -0800

    Create `HDFM` data-generating process type

--- src/simulations/dgp.jl
+++ src/simulations/dgp.jl
@@ -109,4 +109,61 @@ function simulateStateSpaceModel(num_obs, H, A, F, μ, R, Q, Z)
 
     # Return data 
     return data_y, data_z, data_β
-end 
+end
+
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+"""
+
+    HDFM(nlevels, nvar, nfactors, fassign, flags, varlags, varcoefs, fcoefs, fvars, varvars) 
+
+Description:
+Creates an object of type `HDFM` that contains all parameters necessary to specify a multi-level linear dynamic factor data-generating process. 
+
+Inputs: 
+- nlevels = number of levels in the multi-level model structure.
+- nvar = number of variables.
+- nfactors = number of factors for each level (vector of length `nlevels`). 
+- fassign = determines which factor is assigned to which variable for each level (integer matrix of size `nvar` × `nlevels`).
+- flags = number of autoregressive lags for factors of each level (factors of the same level are restricted to having the same number of lags; vector of length `nlevels`).
+- varlags = number of observed variable error autoregressive lags (vector of length `nvar`).
+- varcoefs = vector of coefficients for each variable in the observation equation (length 1+`nlevels`, where first entry represents the intercept). 
+- fcoefs = list of `nlevels` number of matrices, for which each row contains vectors of the autoregressive lag coefficients of the corresponding factor. 
+- fvars = list of `nlevels` number of vectors, where each entry contains the disturbance variance of the corresponding factors.
+- varvars = vector of `nvar` number of entries, where each entry contains the innovation variance of the corresponding variable.
+"""
+@with_kw mutable struct HDFM
+    nlevels::Int64                  # number of levels in the multi-level model structure 
+    nvar::Int64                     # number of variables in the dataset 
+    nfactors::Array{Int64,1}        # number of factors for each level (vector of length `nlevels`)
+    fassign::Array{Int64,2}         # integer matrix of size `nvar` × `nlevels` 
+    flags::Array{Int64,1}           # number of autoregressive lags for each factor level (vector of length `nlevels`)
+    varlags::Array{Int64,1}         # number of obs. variable error autoregressive lags (vector of length `nvar`)
+    varcoefs::Array{Int64,2}        # vector of coefficients for each variables (length 1+`nlevels`)
+    fcoefs::Array{Any,1}            # list of `nlevels` number of matrices, where each row contains vectors of autoregressive lag coefficients of corresponding factors
+    fvars::Array{Any,1}             # list of `nlevels` number of vectors, where each entry contains the disturbance variance of the corresponding factors
+    varvars::Array{Int64,1}         # vector of `nvar` number of entries, where each entry contains innovation variances of corresponding variables
+end;
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
+######################
