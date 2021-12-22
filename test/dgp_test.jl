f6f73cab89e01fc58bbc4067963a509a545c73ed
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-12-22 09:21:56 -0800

    HDFM simulation test

---
+++ test/dgp_test.jl
@@ -0,0 +1,75 @@
+nlevels = 3
+
+nvar = 8
+
+nfactors = [1, 2, 4]
+
+fassign = [1 1 1
+    1 1 1
+    1 1 2
+    1 1 2
+    1 2 3
+    1 2 3
+    1 2 4
+    1 2 4]
+
+flags = [1, 2, 1]
+
+varlags = [2, 2, 2, 2, 2, 2, 2, 2]
+
+varcoefs = [1.0 0.5 0.5 0.5
+    2.0 0.5 0.2 0.5
+    3.0 0.7 0.4 0.4
+    4.0 0.3 0.5 0.3
+    5.0 0.5 0.5 0.1
+    6.0 0.5 0.7 0.2
+    7.0 0.4 0.5 0.5
+    8.0 0.5 0.2 0.5]
+
+varlagcoefs = [0.5 0.25
+    0.5 0.25
+    0.5 0.25
+    0.5 0.25
+    0.5 0.25
+    0.5 0.25
+    0.5 0.25
+    0.5 0.25]
+
+fcoefs = Any[]
+fmat = [0.5][:, :]
+push!(fcoefs, fmat)
+fmat = [0.5 0.25
+    0.5 0.25]
+push!(fcoefs, fmat)
+fmat = [0.5
+    0.5
+    0.5
+    0.5][:, :]
+push!(fcoefs, fmat)
+
+fvars = Any[]
+fmat = [0.1]
+push!(fvars, fmat)
+fmat = [1.5, 1.5]
+push!(fvars, fmat)
+fmat = [1.0, 1.0, 1.0, 1.0]
+push!(fvars, fmat)
+
+varvars = 5 .* rand(8) .^ 2;
+
+hdfm = HDFM(nlevels = nlevels,
+    nvar = nvar,
+    nfactors = nfactors,
+    fassign = fassign,
+    flags = flags,
+    varlags = varlags,
+    varcoefs = varcoefs,
+    varlagcoefs = varlagcoefs,
+    fcoefs = fcoefs,
+    fvars = fvars,
+    varvars = varvars)
+
+ssmodel = convertHDFMtoSS(hdfm)
+
+num_obs = 100
+data_y, data_z, data_β = simulateSSModel(num_obs, ssmodel::SSModel)
