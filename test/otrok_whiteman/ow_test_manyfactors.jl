05aeb5713b2ecbe1cad8aa7a12eedcf20da5cc54
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-01-02 15:54:17 -0800

    Add new OW estimator test

--- test/otrok_whiteman/ow_test_manyfactors.jl
+++ test/otrok_whiteman/ow_test_manyfactors.jl
@@ -3,10 +3,14 @@ nlevels = 2
 
 nvar = 20
 
-nfactors = [1, 2]
+nfactors = [1, 5]
 
 fassign = ones(Int, nvar, 2)
-fassign[(1+trunc(Int, nvar / 2)):end, 2] = 2 * ones(Int, trunc(Int, nvar / 2))
+fassign[1:4, 2] = ones(Int, 4)
+fassign[5:8, 2] = 2 * ones(Int, 4)
+fassign[9:12, 2] = 3 * ones(Int, 4)
+fassign[13:16, 2] = 4 * ones(Int, 4)
+fassign[17:20, 2] = 5 * ones(Int, 4)
 
 flags = [2, 2]
 
@@ -17,26 +21,33 @@ varcoefs[:, 2] = 0.5 * ones(nvar)
 varcoefs[1, 2] = 1.0
 varcoefs[:, 3] = 0.1 * ones(nvar)
 varcoefs[1, 3] = 1.0
-varcoefs[1+trunc(Int, nvar / 2), 3] = 1.0
+varcoefs[5, 3] = 1.0
+varcoefs[9, 3] = 1.0
+varcoefs[13, 3] = 1.0
+varcoefs[17, 3] = 1.0
+
 
 varlagcoefs = ones(nvar, 2)
-varlagcoefs[:, 1] = 0.2 * varlagcoefs[:, 1]
-varlagcoefs[:, 2] = 0.1 * varlagcoefs[:, 2]
+varlagcoefs[:, 1] = 0.5 * varlagcoefs[:, 1]
+varlagcoefs[:, 2] = 0.25 * varlagcoefs[:, 2]
 
 fcoefs = Any[]
 fmat = [0.85 -0.3][:, :]
 push!(fcoefs, fmat)
 fmat = [0.5 0.05
-    0.2 -0.1]
+    0.2 -0.1
+    0.3 -0.1
+    0.4 -0.1
+    0.5 -0.1]
 push!(fcoefs, fmat)
 
 fvars = Any[]
 fmat = [1.0]
 push!(fvars, fmat)
-fmat = [1.0, 1.0]
+fmat = [1.0, 1.0, 1.0, 1.0, 1.0]
 push!(fvars, fmat)
 
-varvars = 1 * ones(nvar);
+varvars = 0.05 * ones(nvar);
 
 hdfm = HDFM(nlevels = nlevels,
     nvar = nvar,
@@ -74,7 +85,7 @@ quant33 = Any[]
 quant66 = Any[]
 stds = Any[]
 
-j = 3
+j = 4
 for i in 1:size(results.F)[1]
     push!(stds, std(results.F[i, j, :]))
     push!(quant33, quantile(results.F[i, j, :], 0.33))
