d378894bcae04fd80ed385d587635719c5b22e25
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-01-20 02:45:42 -0800

    Add 2-level HDFM variance decomposition function

---
+++ src/output_analysis/variance_decomposition.jl
@@ -0,0 +1,29 @@
+function vardecomp2level(data, factor, betas, factorassign)
+
+    T, N = size(data)
+    T, Nf = size(factor)
+
+    # Record data variances 
+    varvars = zeros(N)
+    for i in 1:N
+        varvars[i] = var(data[:, i])
+    end
+
+    # Record factor estimate variances 
+    varfacs = zeros(Nf)
+    for j in 1:Nf
+        varfacs[j] = var(factor[:, j])
+    end
+
+    # Compute variance decompositions 
+    vardecomps = zeros(N, 2)
+    for i in 1:N
+        vardecomps[i, 1] = (betas[i, 2]^2 * varfacs[1]) / varvars[i]
+        vardecomps[i, 2] = (betas[i, 3]^2 * varfacs[1+factorassign[i, 2]]) / varvars[i]
+    end
+
+    return vardecomps
+end
+
+vardecomp = vardecomp2level(datamat, results.means.F, reshape(results.means.B, 3, 50)', fassign)
+
