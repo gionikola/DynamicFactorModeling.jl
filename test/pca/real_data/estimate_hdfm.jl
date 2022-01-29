3bf7645f736e1f77aeb67aaa08835bd9abb438c6
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-01-29 12:27:00 -0800

    Minor changes

--- test/pca/real_data/estimate_hdfm.jl
+++ test/pca/real_data/estimate_hdfm.jl
@@ -14,7 +14,7 @@ using DataFrames
 
 
 # Import data 
-data = readdlm("test/otrok_whiteman/real_data/data_deseasoned_growth.csv", ',')
+data = readdlm("test/pca/real_data/data_deseasoned.csv", ',')
 regstatematch = readdlm("test/otrok_whiteman/real_data/regstatematch.csv", ',')
 
 # Data as matrix 
@@ -22,7 +22,7 @@ datamat = Matrix(data[2:end, :])     # remove first row containing col names
 datamat = Float64.(datamat)         # transform entires to Float64
 
 for i in 1:size(datamat)[2]
-    datamat[:, i] = datamat[:, i] ./ std(datamat[:, i])
+    datamat[:, i] = (datamat[:, i] .- mean(datamat[:,i]) )./ std(datamat[:, i])
 end
 
 regstatematch = Matrix(regstatematch[2:end, :])
@@ -51,8 +51,8 @@ hdfmpriors = HDFMStruct(nlevels = nlevels,
     ndraws = 1000,
     burnin = 50)
 
-results = PCA2LevelEstimator(datamat, hdfmpriors)
-#results2 = KN2LevelEstimator(datamat, hdfmpriors)
+#results = PCA2LevelEstimator(datamat, hdfmpriors)
+results = KN2LevelEstimator(datamat, hdfmpriors)
 
 #Save state names
 statenames = data[1, :]
@@ -62,18 +62,19 @@ quant33 = Any[]
 quant66 = Any[]
 stds = Any[]
 
-j = 4
+j = 1
 for i in 1:size(results.F)[1]
     push!(stds, std(results.F[i, j, :]))
-    push!(quant33, quantile(results.F[i, j, :], 0.05))
-    push!(quant66, quantile(results.F[i, j, :], 0.95))
+    push!(quant33, quantile(results.F[i, j, :], 0.33))
+    push!(quant66, quantile(results.F[i, j, :], 0.66))
     push!(medians, median(results.F[i, j, :]))
 end
 
-plot(results.means.F[:, j][10:end])
+plot(results.means.F[:, j][1:end])
 #plot!(results.means.F[:, 1])
-plot!(quant33[10:end])
-plot!(quant66[10:end])
+plot(medians) 
+plot!(quant33)
+plot!(quant66)
 
 vardecomp = vardecomp2level(datamat, results.means.F, reshape(results.means.B, 3, 50)', fassign)
 #vardecomp2 = vardecomp2level(datamat, results2.means.F, reshape(results2.means.B, 3, 50)', fassign)
