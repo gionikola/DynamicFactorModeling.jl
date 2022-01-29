c54db859461e907b3a4c8d0e48cda41dfdd7847e
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-01-29 13:52:42 -0800

    Create new 1-level KN estimator and archive old

--- src/kim_nelson/archive/kn_1level_estimator.jl
+++ src/kim_nelson/archive/kn_1level_estimator.jl
@@ -54,17 +54,13 @@ function KN1LevelEstimator(data::Array{Float64,2}, dfm::DFMStruct)
     psave2 = zeros(totdraws, nvar * errorlags)      # factor autoregressive polynomials
 
     # Initialize global factor 
-    factor = zeros(nobs, 1)           # Random starting factor series matrix 
-    factor[:, 1], component = firstComponentFactor(y)     # Starting global factor = crosssectional mean of obs. series 
-    if cor(factor[:, 1], y[:, 1]) < 0
-        factor[:, 1] = -factor[:, 1]
-    end
+    factor = mean(y, dims = 2)
 
     # Begin Monte Carlo Loop
     for dr = 1:totdraws
-    
+
         println(dr)
-    
+
         # Create HDFM parameter containers 
         varcoefs = zeros(nvar, 2)[:, :]
         varlagcoefs = zeros(nvar, errorlags)[:, :]
@@ -73,64 +69,58 @@ function KN1LevelEstimator(data::Array{Float64,2}, dfm::DFMStruct)
         fvars = Any[]
         push!(fvars, ones(1))
         varvars = zeros(nvar)
-    
+
         ##################################
         ##################################
         # Draw β, σ2, ϕ
-    
+
         ## Gather all regressors into `X`
         X = [ones(nobs) factor]
-    
+
         ## Initialize β, σ2, ϕ
         β = ones(2)
         σ2 = 0
         ϕ = zeros(errorlags)
-    
+
         ## Iterate over all data series 
         ## to draw obs. eq. hyperparameters 
         for i = 1:nvar
-    
+
             ## Save i-th series 
             Y = y[:, i]
-    
-            ϕold = zeros(errorlags)
-            if dr > 1
-                ϕold = psave2[dr-1, ((i-1)*errorlags)+1:i*errorlags]
-                ϕold = vec(ϕold)
-            end
-    
+
             if i == 1
                 ind = 0
-                β, σ2, ϕ = autocorrErrorLinearRegressionSampler(Y, X, ϕold, errorlags)
+                β, σ2, ϕ = autocorrErrorLinearRegressionSampler(Y, X, errorlags)
                 while β[2] < 0
                     ind += 1
                     ## Draw observation eq. hyperparameters 
-                    β, σ2, ϕ = autocorrErrorLinearRegressionSampler(Y, X, ϕold, errorlags)
+                    β, σ2, ϕ = autocorrErrorLinearRegressionSampler(Y, X, errorlags)
                     if ind >= 100
                         factor = -factor
                         X = [ones(nobs) factor]
                     end
                 end
             else
-                β, σ2, ϕ = autocorrErrorLinearRegressionSampler(Y, X, ϕold, errorlags)
+                β, σ2, ϕ = autocorrErrorLinearRegressionSampler(Y, X, errorlags)
             end
-    
+
             ## Fill out HDFM objects 
             varcoefs[i, :] = β'
             varvars[i] = σ2
             varlagcoefs[i, :] = ϕ'
-    
+
             ## Save observation eq. hyperparameter draws 
             bsave[dr, ((i-1)*nreg)+1:i*nreg] = β'
             ssave[dr, i] = σ2
             psave2[dr, ((i-1)*errorlags)+1:i*errorlags] = ϕ'
-    
+
         end
-    
+
         ##################################
         ##################################
         # Draw factor lag coefficients 
-    
+
         ## Create factor regressor matrix 
         X = zeros(nobs, 1 + factorlags)
         X[:, 1] = ones(nobs)
@@ -138,88 +128,39 @@ function KN1LevelEstimator(data::Array{Float64,2}, dfm::DFMStruct)
             X[:, 1+j] = lag(factor, j, default = 0.0)
         end
         X = X[(factorlags+1):nobs, :]
-    
-        ind = 0
-        accept = 0
-        ψ = zeros(1 + factorlags)
-        while accept == 0
-    
-            ind += 1
-    
-            ## Draw ψ
-            ψ = linearRegressionSamplerRestrictedVariance(factor[(factorlags+1):nobs, 1], X, 1.0)
-    
-            ## Check for stationarity 
-            coef = [-reverse(vec(ψ[2:end]), dims = 1); 1]                      # check stationarity 
-            root = roots(Polynomial(reverse(coef)))
-            rootmod = abs.(root)
-            accept = min(rootmod...) >= 1.01
-    
-            ## If while loop goes on for too long 
-            if ind > 100
-                ψ = psave[dr-1, 1:(1+factorlags)]
-                coef = [-reverse(vec(ψ[2:end]), dims = 1); 1]                      # check stationarity 
-                root = roots(Polynomial(reverse(coef)))
-                rootmod = abs.(root)
-                accept = min(rootmod...) >= 1.01
-            end
-        end
-    
+
+        ## Draw ψ
+        ψ = linearRegressionSamplerRestrictedVariance(factor[(factorlags+1):nobs], X, 1.0)
+
         ## Fill out HDFM objects 
         fcoefs = ψ
-    
+
         ## Save new draw of ψ
         psave[dr, :] = ψ'
-    
+
         ##################################
         ##################################
         # Draw factor  
-    
-        # Size of the state vector 
-        m = factorlags + nvars * errorlags
-    
-        H = zeros(nvars, m)
-        H[:, 1] = betas[:, 2]
-        H[:, 2:2+nvars-1] = I(nvars)
-    
-        A = zeros(nvars, m)
-    
-        F = zeros(m, m)
-        for j in 1:factorlags
-            F[1, 1+(j-1)*nvars] = psis[1, 1+j]
+
+        #draw factor
+        #take drawing of World factor 
+        sinvf1 = sigbig(ψ[2:end], factorlags, nobs)
+        f = zeros(nobs, 1)
+        H = sinvf1' * sinvf1
+
+        for i = 1:nvar
+            sinv1 = sigbig(vec(varlagcoefs[i, :]), errorlags, nobs)
+            H = H + ((varcoefs[i, 2]^2 / varvars[i]) * sinv1' * sinv1)
+            f = f + (varcoefs[i, 2] / varvars[i]) * sinv1' * sinv1 * (y[:, i])
         end
-        for i in 1:nvars
-            for j in 1:errorlags
-                F[1+i, 1+i+(j-1)*nvars] = phis[i, j]
-            end
-        end
-    
-        μ = zeros(m)
-    
-        R = zeros(nvars, nvars)
-    
-        Q = zeros(m, m)
-        Q[1, 1] = 1
-        for i in 1:nvars
-            Q[1+i, 1+i] = sigmas[i]
-        end
-    
-        Z = zeros(nvars, nvars)
-    
-        ssmodel = SSModel(H, A, F, μ, R, Q, Z)
-    
-        data_partial = similar(data)
-    
-        for i in 1:nvars
-            data_partial[:, i] = data[:, i] 
-        end
-    
-        factor = KNFactorSampler(data_partial, ssmodel)
-        factor = factor[:, 1]
-    
+
+        Hinv = inv(H)
+        f = Hinv * f
+        factor = sim_MvNormal(vec(f), Hinv)
+
         ## Save factor 
         Xtsave[:, dr] = factor
-    
+
         println(dr)
     end
 
