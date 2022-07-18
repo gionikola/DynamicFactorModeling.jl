510142eaa7a8d2e6bfcae6cfe12fef1a3da8c385
gionikola <gnikolai@uoregon.edu>
2022-07-18 02:02:52 -0700

    Remove outdated paths and function exports

--- src/DynamicFactorModeling.jl
+++ src/DynamicFactorModeling.jl
@@ -7,7 +7,6 @@ using Statistics
 using Random
 using Distributions
 using PDMats
-#using PDMatsExtras
 using ShiftedArrays
 using Parameters
 using Polynomials
@@ -18,22 +17,15 @@ using Kronecker
 include("simulations/dgp.jl")
 include("common_types/common_types.jl")
 include("linear_regression/linear_regression.jl")
-include("pca/pca_1level_estimator.jl")
-include("pca/pca_2level_estimator.jl")
 include("kim_nelson/kn_1level_estimator.jl")
 include("kim_nelson/kn_2level_estimator.jl")
-include("otrok_whiteman/ow_1level_estimator.jl")
-include("otrok_whiteman/ow_2level_estimator.jl")
 include("output_analysis/variance_decomposition.jl")
 include("test_scripts.jl")
 
 ######################
 # Export package objects 
-export SSModel, HDFM, convertHDFMtoSS, simulateSSModel,
-        DFMStruct, HDFMStruct,
+export SSModel, HDFM, convertHDFMtoSS, simulateSSModel, DFMStruct, HDFMStruct,
         KN1LevelEstimator, KN2LevelEstimator,
-        OW1LevelEstimator, OW2LevelEstimator,
-        PCA1LevelEstimator, PCA2LevelEstimator,
         vardecomp2level,
         sayhi
 end
