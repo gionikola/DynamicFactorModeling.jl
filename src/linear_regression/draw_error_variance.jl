e9a61395ae8f9c015342bd6a3f11d2aeef46e3ed
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-09-27 03:40:03 +0400

    Add new method for `draw_error_variance`

--- src/linear_regression/draw_error_variance.jl
+++ src/linear_regression/draw_error_variance.jl
@@ -14,3 +14,20 @@ function draw_error_variance(Y::Matrix{Float64}, X::Matrix{Float64}, β::Vector{
     ## Return draw of σ2
     return σ2 = Γinv(T1,θ1)::Float64 
 end 
+
+function draw_error_variance(Y::Vector{Float64}, X::Matrix{Float64}, β::Vector{Float64})
+
+    ## Priors
+    T0 = 1
+    θ0 = 0.1
+
+    ## Degrees of freedom parameter posterior
+    T = size(X)[1]
+    T1 = (T0 + T)
+
+    ## Scale parameter posterior
+    θ1 = θ0 + dot((Y - X * β), (Y - X * β))
+
+    ## Return draw of σ2
+    return σ2 = Γinv(T1, θ1)::Float64
+end 
