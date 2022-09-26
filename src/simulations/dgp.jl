c9ea05ba4a42def543e2daed8cf25941050d4cf2
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-09-27 03:06:09 +0400

    Add integer vector and matrix method to `mvn`

--- src/simulations/dgp.jl
+++ src/simulations/dgp.jl
@@ -45,6 +45,39 @@ function mvn(μ::Vector{Float64}, Σ::Matrix{Float64})
     return X::Vector{Float64}
 end
 
+function mvn(μ::Vector{Int}, Σ::Matrix{Int})
+
+    nvar  = size(Σ)[1]         # Num. of variables 
+
+    if (0 in diag(Σ)) == false  # No degenerate random vars.
+        Q = cholesky(Hermitian(Σ), Val(true), check = false).U       # Upper triang. Cholesky mat.  
+        X = Q * randn(length(μ)) + μ    # Multiv. normal vector draw  
+    else                        # in case of degenerate random vars.
+        keep = Any[] 
+        for i in 1:nvar
+            if Σ[i,i] != 0
+                push!(keep, i)
+            end 
+        end  
+        Σsub = Σ[keep, keep] 
+        μsub = μ[keep] 
+        Q = cholesky(Hermitian(Σsub), Val(true), check = false).U       # Upper triang. Cholesky mat.  
+        Xsub = Q * randn(length(μsub)) + μsub    # Multiv. normal vector draw  
+        X = zeros(nvar) 
+        j = 1
+        for i in 1:nvar
+            if i in keep    # If i-th var. is non-degen. 
+                X[i] = Xsub[j]
+                j = j + 1   
+            else
+                X[i] = μ[i] # If i-th var. is degen. 
+            end 
+        end 
+    end 
+
+    return X::Vector{Float64}
+end
+
 function mvn(μ::Float64, Σ::Float64)
 
     if Σ != 0  # No degenerate random vars.     # Upper triang. Cholesky mat.  
