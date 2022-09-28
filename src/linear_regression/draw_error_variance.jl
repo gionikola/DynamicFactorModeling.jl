2c8022695c2d91809f4dfd40e9d6026c76f83786
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-09-28 17:00:44 +0400

    Add new `draw_error_variance()` methods

--- src/linear_regression/draw_error_variance.jl
+++ src/linear_regression/draw_error_variance.jl
@@ -32,6 +32,57 @@ function draw_error_variance(Y::Vector{Float64}, X::Matrix{Float64}, β::Vector{
     return σ2 = Γinv(T1, θ1)::Float64
 end 
 
+function draw_error_variance(Y::Matrix{Float64}, X::Vector{Float64}, β::Vector{Float64})
+
+    ## Priors
+    T0 = 1
+    θ0 = 0.1
+
+    ## Degrees of freedom parameter posterior
+    T = length(X)
+    T1 = (T0 + T)
+
+    ## Scale parameter posterior
+    θ1 = θ0 + dot((Y - X * β[1]), (Y - X * β[1]))
+
+    ## Return draw of σ2
+    return σ2 = Γinv(T1, θ1)::Float64
+end 
+
+function draw_error_variance(Y::Matrix{Float64}, X::Vector{Float64}, β::Float64)
+
+    ## Priors
+    T0 = 1
+    θ0 = 0.1
+
+    ## Degrees of freedom parameter posterior
+    T = length(X)
+    T1 = (T0 + T)
+
+    ## Scale parameter posterior
+    θ1 = θ0 + dot((Y - X * β), (Y - X * β))
+
+    ## Return draw of σ2
+    return σ2 = Γinv(T1, θ1)::Float64
+end 
+
+function draw_error_variance(Y::Vector{Float64}, X::Vector{Float64}, β::Float64)
+
+    ## Priors
+    T0 = 1
+    θ0 = 0.1
+
+    ## Degrees of freedom parameter posterior
+    T = length(X)
+    T1 = (T0 + T)
+
+    ## Scale parameter posterior
+    θ1 = θ0 + dot((Y - X * β), (Y - X * β))
+
+    ## Return draw of σ2
+    return σ2 = Γinv(T1, θ1)::Float64
+end 
+
 function draw_error_variance(Y::Vector{Float64}, X::Vector{Float64}, β::Vector{Float64})
 
     ## Priors
