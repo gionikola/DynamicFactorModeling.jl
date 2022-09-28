db1ee187157590cc1f7348ca9f157e84a5a6ae66
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-09-28 17:01:57 +0400

    Add new parameter draw tests

--- test/parameter_draws.jl
+++ test/parameter_draws.jl
@@ -1,5 +1,5 @@
 
-@testset "draw β coefficients" begin
+@testset "Draw β coefficients" begin
 
     Y = rand(100)
     X = rand(100, 2)
@@ -17,6 +17,14 @@
     @test typeof(β) == Vector{Float64}
     @test size(β) == (2,)
 
+    Y = rand(100)[:,:]
+    X = rand(100)
+    σ2 = 2.0
+    β = DynamicFactorModeling.draw_coefficients(Y, X, σ2)
+
+    @test typeof(β) == Float64
+    @test size(β) == ()
+
     Y = rand(100)
     X = rand(100)
     σ2 = 2.0
@@ -27,7 +35,7 @@
 
 end
 
-@testset "draw σ2" begin
+@testset "Draw σ2" begin
 
     Y = rand(100)
     X = rand(100, 2)
@@ -53,5 +61,41 @@ end
 
     @test typeof(σ2) == Float64
     @test size(σ2) == ()
+
+end
+
+@testset "Draw regression parameters" begin
+
+    Y = rand(100)[:, :]
+    X = rand(100, 2)
+    σ2 = 1.0
+    β, σ2 = DynamicFactorModeling.draw_parameters(Y, X, σ2)
+
+    @test typeof(β) == Vector{Float64}
+    @test size(β) == (2,)
+
+    Y = rand(100)
+    X = rand(100, 2)
+    σ2 = 1.0
+    β, σ2 = DynamicFactorModeling.draw_parameters(Y, X, σ2)
+
+    @test typeof(β) == Vector{Float64}
+    @test size(β) == (2,)
+
+    Y = rand(100)[:, :]
+    X = rand(100)
+    σ2 = 1.0
+    β, σ2 = DynamicFactorModeling.draw_parameters(Y, X, σ2)
+
+    @test typeof(β) == Float64
+    @test size(β) == ()
+
+    Y = rand(100)
+    X = rand(100)
+    σ2 = 1.0
+    β, σ2 = DynamicFactorModeling.draw_parameters(Y, X, σ2)
+
+    @test typeof(β) == Float64
+    @test size(β) == ()
 
 end 
