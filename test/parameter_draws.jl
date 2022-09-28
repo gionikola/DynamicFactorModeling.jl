26e96582d10e84479a31b6ca26624d6aaf22f89f
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-09-28 15:49:25 +0400

    Add methods and tests for coeff and var draws

--- test/parameter_draws.jl
+++ test/parameter_draws.jl
@@ -9,15 +9,37 @@
     @test typeof(β) == Vector{Float64}
     @test size(β) == (2,)
 
-end
+    Y = rand(100)[:, :]
+    X = rand(100, 2)
+    σ2 = 2.0
+    β = DynamicFactorModeling.draw_coefficients(Y, X, σ2)
 
+    @test typeof(β) == Vector{Float64}
+    @test size(β) == (2,)
+
+    Y = rand(100)
+    X = rand(100)
+    σ2 = 2.0
+    β = DynamicFactorModeling.draw_coefficients(Y, X, σ2)
+
+    @test typeof(β) == Float64
+    @test size(β) == ()
+
+end
 
 @testset "draw σ2" begin
 
     Y = rand(100)
     X = rand(100, 2)
     β = [1.0, 1.0]
-    
+
+    σ2 = DynamicFactorModeling.draw_error_variance(Y, X, β)
+
+    @test typeof(σ2) == Float64
+    @test size(σ2) == ()
+
+    Y = rand(100)[:, :]
+
     σ2 = DynamicFactorModeling.draw_error_variance(Y, X, β)
 
     @test typeof(σ2) == Float64
